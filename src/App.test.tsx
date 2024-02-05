f417dee34c64388cc373658cabc1518a4d757c3c
omerrkosar <omerkosar382@gmail.com>
2024-02-05 13:45:13 +0300

    add tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,74 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Add and Remove Option', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // ACT
+  userEvent.click(screen.getByPlaceholderText('Select...'));
+  await screen.findByText('Rick Sanchez');
+
+  userEvent.click(screen.getByText('Rick Sanchez'));
+  expect(screen.queryAllByRole('button')).toHaveLength(1);
+  userEvent.keyboard('{Backspace}');
+  expect(screen.queryAllByRole('button')).toHaveLength(0);
+});
+
+test('Keyboard Test', async () => {
+  window.HTMLElement.prototype.scrollIntoView = function () {
+    //for test
+  };
+
+  render(<App />);
+  // ACT
+  userEvent.click(screen.getByPlaceholderText('Select...'));
+  await screen.findByText('Rick Sanchez');
+
+  userEvent.keyboard('{ArrowDown}');
+  userEvent.keyboard('{ArrowDown}');
+  userEvent.keyboard('{ArrowDown}');
+  userEvent.keyboard('{ArrowDown}');
+  userEvent.keyboard('{ArrowDown}');
+  userEvent.keyboard('{ArrowUp}');
+  userEvent.keyboard('{Enter}');
+  userEvent.keyboard('{ArrowDown}');
+  userEvent.keyboard('{ArrowDown}');
+  userEvent.keyboard('{Enter}');
+
+  expect(screen.queryAllByRole('button')).toHaveLength(2);
+  userEvent.keyboard('{ArrowUp}');
+  userEvent.keyboard('{ArrowUp}');
+  userEvent.keyboard('{ArrowUp}');
+  userEvent.keyboard('{ArrowUp}');
+  userEvent.keyboard('{ArrowUp}');
+  userEvent.keyboard('{Enter}');
+  expect(screen.queryAllByRole('button')).toHaveLength(3);
+  userEvent.keyboard('{ArrowDown}');
+  userEvent.keyboard('{ArrowDown}');
+  userEvent.keyboard('{ArrowDown}');
+  userEvent.keyboard('{Enter}');
+  expect(screen.queryAllByRole('button')).toHaveLength(2);
+  userEvent.keyboard('{Enter}');
+  expect(screen.queryAllByRole('button')).toHaveLength(3);
+  userEvent.keyboard('{ArrowLeft}');
+  userEvent.keyboard('{ArrowLeft}');
+  userEvent.keyboard('{Backspace}');
+  expect(screen.queryAllByRole('button')).toHaveLength(2);
+  userEvent.keyboard('{Backspace}');
+  userEvent.keyboard('{Backspace}');
+  expect(screen.queryAllByRole('button')).toHaveLength(0);
+});
+
+test('Click Checkbox', async () => {
+  render(<App />);
+  // ACT
+  userEvent.click(screen.getByPlaceholderText('Select...'));
+  await screen.findByText('Rick Sanchez');
+
+  userEvent.click(screen.getAllByRole('checkbox')[0]);
+  expect(screen.queryAllByRole('button')).toHaveLength(1);
+  userEvent.click(screen.getAllByRole('checkbox')[0]);
+  expect(screen.queryAllByRole('button')).toHaveLength(0);
 });
